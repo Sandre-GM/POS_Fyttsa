1b3aff00e3e613a1280e69c9e308ff00067a36d4
Mauricio Gonzalez Marmolejo <sandre9477@gmail.com>
2025-11-19 17:52:39 -0600

    stack of development

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center min-h-screen">
+      <h1>Hello fyttsa</h1>
+      <Button variant="outline" size="lg">
+        Let's do it
+      </Button>
+    </div>
+  );
+}
+
+export default App;
